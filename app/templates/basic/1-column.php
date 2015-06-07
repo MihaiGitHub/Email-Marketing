2aab158c48b7edaea3f903b2b71dd7d1171d2b27
Mihai <mihailiviu@yahoo.com>
2015-06-06 22:15:08 -0700

    Update for the 1-column template

--- app/templates/basic/1-column.php
+++ app/templates/basic/1-column.php
@@ -530,30 +530,12 @@
                                     <table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateFooter">
                                         <tr>
                                             <td valign="top" class="footerContent" mc:edit="footer_content00">
-                                             <div id="block5" contenteditable="true" class="editable">
-                                                <a href="#">Follow on Twitter</a>&nbsp;&nbsp;&nbsp;<a href="#">Friend on Facebook</a>&nbsp;&nbsp;&nbsp;<a href="#">Forward to Friend</a>&nbsp;
+                                             <div id="block5" contenteditable="true" class="editable" style="float:left;">
+                                                <a href="#">Follow on Twitter</a>&nbsp;&nbsp;&nbsp;<a href="#">Friend on Facebook</a>
                                               </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td valign="top" class="footerContent" style="padding-top:0;" mc:edit="footer_content01">
-                                            <!--
-                                             <div id="footer" contenteditable="true">
-                                                <em>Copyright &copy; *|CURRENT_YEAR|* *|LIST:COMPANY|*, All rights reserved.</em>
-                                                <br />
-                                                *|IFNOT:ARCHIVE_PAGE|* *|LIST:DESCRIPTION|*
-                                                <br />
-                                                <br />
-                                                <strong>Our mailing address is:</strong>
-                                                <br />
-                                                *|HTML:LIST_ADDRESS_HTML|* *|END:IF|* 
+                                              <div style="float:right;">
+                                              	<a href="#">unsubscribe from this list</a>
                                               </div>
-                                              -->
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td valign="top" class="footerContent" style="padding-top:0; padding-bottom:40px;" mc:edit="footer_content02">
-                                            	<a href="#">unsubscribe from this list</a>&nbsp;&nbsp;&nbsp;<a href="#">update subscription preferences</a>&nbsp;
                                             </td>
                                         </tr>
                                     </table>
