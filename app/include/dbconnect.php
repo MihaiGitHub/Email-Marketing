53692b05283c694cb5e12efa80312954e7b12e38
Mihai <mihailiviu@yahoo.com>
2015-07-02 16:30:45 -0700

    Update for connection file

--- app/include/dbconnect.php
+++ app/include/dbconnect.php
@@ -1,10 +1,10 @@
 <?php
 define( 'THIS_WEBSITE_URI', 'http://' . $_SERVER['HTTP_HOST'] . dirname( $_SERVER['REQUEST_URI'] ) );
 
-$host = "msmarandache.com.mysql";
-$dbname = "msmarandache_co";
-$user = "msmarandache_co";
-$pass = "wurus6Tw";
+$host = "xxxxxxxxxxx";
+$dbname = "xxxxxxxxxxx";
+$user = "xxxxxxxxx";
+$pass = "xxxxxxxxx";
 /* mihaismarandache.com 
 $host = "db543846776.db.1and1.com";
 $dbname = "db543846776";
