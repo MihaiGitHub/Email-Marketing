2c8564f849a20c6b6dc7b4fd9eed203f6a49fbe2
Mihai <mihailiviu@yahoo.com>
2015-07-19 18:27:34 -0700

    Added create custom template page and newsletter builder

---
+++ app/create.php
@@ -0,0 +1,55 @@
+<?php 
+ob_start(); 
+session_start();
+$dashboard = true;
+?>
+		<!-- BEGIN PAGE -->
+		<div id="main-content">
+			<!-- BEGIN PAGE CONTAINER-->
+			<div class="container-fluid">
+				<!-- BEGIN PAGE HEADER-->
+				<div class="row-fluid">
+					<div class="span12">
+						
+						<!-- BEGIN PAGE TITLE & BREADCRUMB-->
+						<h3 class="page-title">
+							Template Builder
+							<small> Create custom template </small>
+						</h3>
+						<ul class="breadcrumb">
+							<li>
+                                <a href="#"><i class="icon-home"></i></a><span class="divider">&nbsp;</span>
+							</li>
+                            
+							<li><a href="#">Builder</a><span class="divider-last">&nbsp;</span></li>
+                            
+						</ul>
+						<!-- END PAGE TITLE & BREADCRUMB-->
+					</div>
+				</div>
+				<!-- END PAGE HEADER-->
+				<!-- BEGIN PAGE CONTENT-->
+				<div id="page" class="dashboard">
+                    <!-- BEGIN OVERVIEW STATISTIC BLOCKS-->
+                    <div class="row-fluid" style="height:850px;">
+                    
+                    
+<iframe frameBorder="0" src="http://msmarandache.com/emarketing/app/assets/template-builder/index.html" style="height:100%;width:100%;"></iframe>
+                    
+
+                    </div>
+                    <!-- END OVERVIEW STATISTIC BLOCKS-->
+					
+				</div>
+				<!-- END PAGE CONTENT-->
+			</div>
+			<!-- END PAGE CONTAINER-->
+		</div>
+		<!-- END PAGE -->
+<?php		  
+$content = ob_get_contents();
+ob_end_clean();
+include 'include/header.php';
+print $content;
+include 'include/footer.php'; 
+?>
