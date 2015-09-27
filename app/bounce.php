ece62fdd19a2b918249eab9f004cb39a37be6f03
unknown <mihailiviu@yahoo.com>
2015-09-27 10:27:04 -0700

    Included bounce error code

--- app/bounce.php
+++ app/bounce.php
@@ -13,14 +13,15 @@ if($emails) {
 	foreach($emails as $email_number) {
 		
 		$message = imap_fetchbody($inbox,$email_number,2);
+		$code = explode('Diagnostic-Code: smtp;', $message);
 		
 		$pattern="/(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/";
-
+		
 		preg_match_all($pattern, $message, $matches);
 		
 		foreach($matches[0] as $email){
-				$stmt = $conn->prepare('UPDATE campaign_emails SET bounced = 1 WHERE email = :email');
-				$result = $stmt->execute(array('email' => $email));
+				$stmt = $conn->prepare('UPDATE campaign_emails SET bounced = 1, error_code = :code WHERE email = :email');
+				$result = $stmt->execute(array('code' => $code[1], 'email' => $email));
 				
 				if($result){
 					imap_delete($inbox, $email_number);
