3a4a9c6a79e2002d69b50b4dc309323cdbda7f76
unknown <mihailiviu@yahoo.com>
2015-10-24 17:22:44 -0700

    Added custom PayPal buttons

--- app/account.php
+++ app/account.php
@@ -41,9 +41,6 @@ $row = $stmt->fetch();
                     <!-- BEGIN OVERVIEW STATISTIC BLOCKS-->
                     <div class="row-fluid circle-state-overview">
                     
-                    
-                    
-                    
                <div class="span12">
                   <div class="widget">
                         <div class="widget-title">
@@ -51,12 +48,6 @@ $row = $stmt->fetch();
                                               
                         </div>
                         <div class="widget-body">
-                        <!--    <div class="row-fluid">
-                                <div class="span12">
-                                    <img src="img/vector-lab.jpg" alt="">
-                                </div>
-                            </div> 
-                            <div class="space20"></div>-->
                             <div class="row-fluid invoice-list">
                                 <div class="span4">
                                     <h5>BILLING ADDRESS</h5>
@@ -68,26 +59,6 @@ $row = $stmt->fetch();
 								<?php echo $row['payer_email']; ?>
                                     </p>
                                 </div>
-						  <!--
-                                <div class="span4">
-                                    <h5>SHIPPING ADDRESS</h5>
-                                    <p>
-                                        Vector Lab<br>
-                                        Road 1, House 2, Sector 3<br>
-                                        ABC, Dreamland 1230<br>
-                                        P: +38 (123) 456-7890<br>
-                                    </p>
-                                </div>
-                                <div class="span4">
-                                    <h5>INVOICE INFO</h5>
-                                    <ul class="unstyled">
-                                        <li>Invoice Number		: <strong>69626</strong></li>
-                                        <li>Invoice Date		: 2013-03-17</li>
-                                        <li>Due Date			: 2013-03-20</li>
-                                        <li>Invoice Status		: Paid</li>
-                                    </ul>
-                                </div>
-						  -->
                             </div>
                             <div class="space20"></div>
                             <div class="space20"></div>
@@ -129,25 +100,33 @@ while($row = $stmt->fetch()){
                             <div class="space20"></div>
                             <div class="row-fluid text-center">
 					   
-<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_blank">
 	<input type="hidden" name="cmd" value="_s-xclick">
 	<input type="hidden" name="hosted_button_id" value="RMJATPW2E8EGY">
 	<table>
-	<tr><td><input type="hidden" name="on0" value="Upgrade">Upgrade</td></tr><tr><td><select name="os0">
-		<option value="5 Email Credits">5 Email Credits $5.00 USD</option>
-		<option value="10 Email Credits">10 Email Credits $10.00 USD</option>
-	</select> </td></tr>
+		<tr>
+			<td>
+				<input type="hidden" name="on0" value="Upgrade">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<select name="os0">
+					<option value="5 Email Credits">5 Email Credits $5.00 USD</option>
+					<option value="10 Email Credits">10 Email Credits $10.00 USD</option>
+				</select> 
+			</td>
+		</tr>
 	</table>
 	<input type="hidden" name="currency_code" value="USD">
-	<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+	<input type="submit" value="Upgrade" name="submit" title="PayPal - The safer, easier way to pay online!" class="paypal_btn">
+	<!--<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">-->
 	<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
-
-					   <!--
-                                <a class="btn btn-primary btn-large hidden-print">Upgrade <i class="m-icon-big-swapright m-icon-white"></i></a>
-						  -->
+<!--
                                <a onclick="javascript:window.print();" class="btn btn-success btn-large hidden-print">Print <i class="icon-print icon-big"></i></a>
-                            </div>
+                            -->
+					   </div>
                         </div>
                   </div>
                </div>
