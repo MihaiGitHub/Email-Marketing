e3971fa3adf4251beb517c013aad8d74c1ada202
unknown <mihailiviu@yahoo.com>
2015-12-05 22:07:07 -0700

    Added confirm password field

--- register.php
+++ register.php
@@ -1,3 +1,10 @@
 ﻿<?php
-echo $_POST['email'];
+include 'app/include/dbconnect.php';
+
+$stmt = $conn->prepare('INSERT INTO users (username, password, role, emails) VALUES (:username, :password, :role, :emails)');
+$result = $stmt->execute(array('username' => $_POST['username'], 'password' => $_POST['password'], 'role' => 'buyer', 'emails' => 5));
+
+if($result){
+	echo json_encode('success');
+}
 ?>
